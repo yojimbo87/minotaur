43bcb6c5b0546ab43a40f44e40da28fb2e26cd4b
Tomi <bosak.tomas@gmail.com>
2011-04-10 12:30:19 +0200

    Simple polling revisited.

--- examples/chat/client.js
+++ examples/chat/client.js
@@ -5,6 +5,21 @@ function debug(message) {
 $(document).ready(function () {
     debug("Ready<br />");
 	
+	minitaur.on("connect", function() {
+        debug("on connect ");
+    });
+    
+    minitaur.on("message", function(data) {
+		if(data && data.cmd) {
+			debug("on message " + data.cmd);
+		}
+    });
+	
+    minitaur.on("disconnect", function() {
+        debug("on disconnect");
+        //setTimeout(minitaur.connect, minitaur.RECONNECT_TIMEOUT);
+    });
+	
 	minitaur.connect();
 });
 
